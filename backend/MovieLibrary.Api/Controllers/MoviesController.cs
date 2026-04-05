854a9fbc9133c40c2d7f3b5fd17ba01fb6f97c9f
Toth Judit <tothjudit.ditta@gmail.com>
2026-04-05 10:50:06 +0100

    Add PUT and DELETE endpoints to MoviesController

--- backend/MovieLibrary.Api/Controllers/MoviesController.cs
+++ backend/MovieLibrary.Api/Controllers/MoviesController.cs
@@ -7,64 +7,40 @@ namespace MovieLibrary.Api.Controllers;
 [Route("api/[controller]")]
 public class MoviesController : ControllerBase
 {
+    private static readonly List<Movie> Movies =
+    [
+        new Movie
+        {
+            Id = "1",
+            Title = "Inception",
+            Director = "Christopher Nolan",
+            Genre = "Sci-Fi",
+            ReleaseYear = 2010,
+            Rating = 8.8,
+            Description = "A skilled thief enters dreams to steal secrets."
+        },
+        new Movie
+        {
+            Id = "2",
+            Title = "The Matrix",
+            Director = "The Wachowskis",
+            Genre = "Sci-Fi",
+            ReleaseYear = 1999,
+            Rating = 8.7,
+            Description = "A hacker discovers the nature of reality."
+        }
+    ];
+
     [HttpGet]
     public ActionResult<IEnumerable<Movie>> GetAll()
     {
-        var movies = new List<Movie>
-        {
-            new Movie
-            {
-                Id = "1",
-                Title = "Inception",
-                Director = "Christopher Nolan",
-                Genre = "Sci-Fi",
-                ReleaseYear = 2010,
-                Rating = 8.8,
-                Description = "A skilled thief enters dreams to steal secrets."
-            },
-            new Movie
-            {
-                Id = "2",
-                Title = "The Matrix",
-                Director = "The Wachowskis",
-                Genre = "Sci-Fi",
-                ReleaseYear = 1999,
-                Rating = 8.7,
-                Description = "A hacker discovers the nature of reality."
-            }
-        };
-
-        return Ok(movies);
+        return Ok(Movies);
     }
 
     [HttpGet("{id}")]
     public ActionResult<Movie> GetById(string id)
     {
-        var movies = new List<Movie>
-        {
-            new Movie
-            {
-                Id = "1",
-                Title = "Inception",
-                Director = "Christopher Nolan",
-                Genre = "Sci-Fi",
-                ReleaseYear = 2010,
-                Rating = 8.8,
-                Description = "A skilled thief enters dreams to steal secrets."
-            },
-            new Movie
-            {
-                Id = "2",
-                Title = "The Matrix",
-                Director = "The Wachowskis",
-                Genre = "Sci-Fi",
-                ReleaseYear = 1999,
-                Rating = 8.7,
-                Description = "A hacker discovers the nature of reality."
-            }
-        };
-
-        var movie = movies.FirstOrDefault(m => m.Id == id);
+        var movie = Movies.FirstOrDefault(m => m.Id == id);
 
         if (movie is null)
         {
@@ -73,4 +49,47 @@ public class MoviesController : ControllerBase
 
         return Ok(movie);
     }
+
+    [HttpPost]
+    public ActionResult<Movie> Create(Movie movie)
+    {
+        movie.Id = (Movies.Count + 1).ToString();
+        Movies.Add(movie);
+
+        return CreatedAtAction(nameof(GetById), new { id = movie.Id }, movie);
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult<Movie> Update(string id, Movie updatedMovie)
+    {
+        var movie = Movies.FirstOrDefault(m => m.Id == id);
+
+        if (movie is null)
+        {
+            return NotFound();
+        }
+
+        movie.Title = updatedMovie.Title;
+        movie.Director = updatedMovie.Director;
+        movie.Genre = updatedMovie.Genre;
+        movie.ReleaseYear = updatedMovie.ReleaseYear;
+        movie.Rating = updatedMovie.Rating;
+        movie.Description = updatedMovie.Description;
+
+        return Ok(movie);
+    }
+    [HttpDelete("{id}")]
+public IActionResult Delete(string id)
+{
+    var movie = Movies.FirstOrDefault(m => m.Id == id);
+
+    if (movie is null)
+    {
+        return NotFound();
+    }
+
+    Movies.Remove(movie);
+
+    return NoContent();
+}
 }
